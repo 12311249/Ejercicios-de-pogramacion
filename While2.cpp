cecd608d25c74acc456f37ef9f2a1f3e29b4ddf9
12311249 <lgonzalezs14@miumg.edu.gt>
2025-02-18 23:18:23 -0600

    mas ejercicios

---
+++ While2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+int n, i = 1;
+
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    while(i <= n) {
+        cout << i << " ";
+        i++;
+    }
+
+
+
+    return 0;
+}
