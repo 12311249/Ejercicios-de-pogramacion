8fd2eeb63c5808acbf8383b856697620ee03cc2e
12311249 <lgonzalezs14@miumg.edu.gt>
2025-02-18 21:01:11 -0600

    Mas ejercicios

--- MULTIPLOS.cpp
+++ MULTIPLOS.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout<<"Ingrese un numero please ";
+    cin>>n;
+
+    if(n % 5 == 0 && n % 7 == 0) {
+        cout<< "El numero "<<n  << " si es multiplo de 5 y 7 "<< endl;
+
+    } else {
+        cout<< "El numero "<<n  << " no es multiplo de 5 y 7 "<< endl;
+
+    }
 
 
 
