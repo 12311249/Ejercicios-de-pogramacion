8b5631ca3ef230152744ce9c9bf0ab4768ff3485
12311249 <lgonzalezs14@miumg.edu.gt>
2025-02-20 20:32:36 -0600

    Este estaba algo corto

---
+++ NATURALES.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    while (i <= 10) {
+        cout <<"Numero :" << i << "| cuadrado : " << i * i << endl;
+        i++;
+    }
+
+
+
+    return 0;
+}
