c6a12cada3418425055f19a2e475eaef6bc4fa27
12311249 <lgonzalezs14@miumg.edu.gt>
2025-02-20 22:32:17 -0600

    NUEVA UPDATE

---
+++ FACTORIAL.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int f;
+    long long factorial = 1;
+
+    cout <<"Ingrese un numero : ";
+    cin >> f;
+
+    if(f<1) {
+        cout<<"El numero es mayor a 0 ";
+    }else {
+        for (int i = 1; i <= f; i++) {
+            factorial *= i;
+        }
+    }
+    
+    return 0;
+}
